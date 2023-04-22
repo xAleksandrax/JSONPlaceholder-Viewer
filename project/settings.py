e26b8d499b44be56628fa8cc61ed934ffe88e7f0
xAleksandrax <aleksandramodrzejewska2001@gmail.com>
2023-04-22 13:17:42 +0200

    fixes added

--- project/settings.py
+++ project/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-&do&dn%hkw-!1us157u)fe&xn0sb0%f(6$*^jnlw%rp_0r7(!h
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['.vercel.app', '127.0.0.1']
 
 
 # Application definition
